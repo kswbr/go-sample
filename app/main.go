f2c681d61b25dee3dc6e4474ea6e9f9c2fd73666
kashiwabara <m_kashiwabara@bbmedia.co.jp>
2018-10-30 15:18:40 +0900

    監視処理追加

--- app/main.go
+++ app/main.go
@@ -1,15 +1,15 @@
 package main
 
-import(
-    "fmt"
-    "net/http"
+import (
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, from Docker container!")
+	fmt.Fprintf(w, "Hello, from Docker container!")
 }
